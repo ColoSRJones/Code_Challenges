a34642110e6e19c13209ca2054f0e1a0a5602070
Steven Jones <colosrjones@gmail.com>
2020-02-26 17:02:07 -0600

    Create greetaliens.js

---
+++ greetaliens.js
@@ -0,0 +1,15 @@
+const greetAliens = arr => {
+      for (let i = 0; i < arr.length; i++) {
+            console.log('Oh powerful ' + arr[i] + ', we humans offer our unconditional surrender!');
+      }
+}
+
+const aliens = ["Blorgous", "Glamyx", "Wegord", "SpaceKing"];
+
+greetAliens(aliens)
+
+// Should print:
+// Oh powerful Blorgous, we humans offer our unconditional surrender! 
+// Oh powerful Glamyx, we humans offer our unconditional surrender! 
+// Oh powerful Wegord, we humans offer our unconditional surrender! 
+// Oh powerful SpaceKing, we humans offer our unconditional surrender! 
